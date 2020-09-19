12241ebbd1be530b6c1c037867575eb7897c5245
Sergey Firsov <fearthunder@yandex.ru>
2020-09-19 23:26:27 +0300

    Add base huification algorithm

--- Huification/app/src/main/java/com/example/huification/MainActivity.kt
+++ Huification/app/src/main/java/com/example/huification/MainActivity.kt
@@ -2,20 +2,48 @@ package com.example.huification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private val xue:String = "Хуе"
-    private val stops:Array<String> = arrayOf("о", "и", "у")
+    private val stops = setOf('о', 'и', 'у', 'е')
+
+    private var xuiresult:TextView? = null
+    private var xuificateBtn:Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        xuiresult = findViewById(R.id.xuiresult)
+        xuificateBtn = findViewById(R.id.xuificate)
     }
 
-    fun xuificate(text:String) {
+    fun xuificate(text:String): String {
 
-        var textSplited = text.split(*stops)
+        if (text.isEmpty()){
+            return "";
+        }
 
+        var index = 0
+        for (char in text) {
+            if (stops.contains(char)){
+                break;
+            }
+            index++
+        }
 
+        if (index + 1 >= text.length)
+        {
+            return xue + text;
+        }
+
+        return xue + text.substring(index + 1, text.length)
+    }
+
+    fun onClickHuificate(view: View){
+        xuiresult?.setText(xuificate(xuiresult?.text.toString()))
     }
 }
